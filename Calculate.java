93f27f59a5d4bb0a7185bbc76e86e9d104b56378
matheuslautert <129200313+matheuslautert@users.noreply.github.com>
2023-09-06 17:14:24 -0300

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,30 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if (args[0].equals("somar")) {
+            sum(x, y);
+        } else if (args[0].equals("subtrair")) {
+            minus(x, y);
+        }else if (args[0].equals("dividir")) {
+            division(x, y);
+        }else if (args[0].equals("multiplicar")) {
+                multiplication(x, y);
+        } else {
+            System.out.print("Nenhuma instrução definida");
+        }
+    }
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+    static void minus(int x, int y) {
+        System.out.println(x-y);
+    }
+    static void division(int x, int y) {
+        System.out.println(x / y);
+    }
+    static void multiplication(int x, int y) {
+        System.out.println(x * y);
+    }
+}
