3f62eb216da41bf9fdd09017a1d186111e8fa292
matheuslautert <129200313+matheuslautert@users.noreply.github.com>
2023-09-06 17:15:25 -0300

    Add files via upload

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args) {
+        //{func(5, 4);
+       // func(7, 4);
+       // func(8, 4);
+        //func(9, 4);
+      //  func(10, 4);
+        //func(100000, 4);
+        System.out.println("Olá" + args[0]);
+    }
+
+    static void func(int x, int y) {
+        System.out.println(x / y);
+
+    }
+}
+      //  var login = "Matheus";
+      // String login1 = "Matheus1";
+      //  int number = 7;
+      //  double fracionado = 1.0870;
+        // boolean MaiorDe18 = true;
+        //boolean Menorde18 = false;
+
+        //System.out.println(login + login1);
+       // System.out.println(login1);
+        //System.out.println(number);
+        //System.out.println(fracionado);
+        //if (MaiorDe18 = true) {
+         //  System.out.println("Maior de idade");
+        //}
+
+
+
+
+
+ //   }
+//}
